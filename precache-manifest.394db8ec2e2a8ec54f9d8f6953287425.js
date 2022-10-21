f015ec3021834e012176d6f982215a245207aea3
mats-js <100519629+mats-js@users.noreply.github.com>
2022-10-21 09:35:53 +0200

    Updates

---
+++ precache-manifest.394db8ec2e2a8ec54f9d8f6953287425.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5c9a504dc00282a4a2",
+    "url": "/meet/static/css/main.003b5405.chunk.css"
+  },
+  {
+    "revision": "5d5c9a504dc00282a4a2",
+    "url": "/meet/static/js/main.f2fd2c3c.chunk.js"
+  },
+  {
+    "revision": "a27224301e031518ea6a",
+    "url": "/meet/static/js/runtime~main.7e263fcc.js"
+  },
+  {
+    "revision": "f338f0462fffe7413605",
+    "url": "/meet/static/js/2.6ef96e32.chunk.js"
+  },
+  {
+    "revision": "307b298eaad73106f9a9",
+    "url": "/meet/static/js/3.b0609f3e.chunk.js"
+  },
+  {
+    "revision": "c895b4d1660c7315caac6270e1c0b580",
+    "url": "/meet/index.html"
+  }
+];
